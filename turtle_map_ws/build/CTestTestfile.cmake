e496e0da449a2c84de86da72b8bf477a799ec8a7
Rasya <school.rasya@gmail.com>
2024-11-02 03:26:14 +0700

    Add files via upload

---
+++ turtle_map_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/rasya/Documents/ROBOTIK/ROS/tugas_turtle/turtle_map_ws/src
+# Build directory: /home/rasya/Documents/ROBOTIK/ROS/tugas_turtle/turtle_map_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot_package")
