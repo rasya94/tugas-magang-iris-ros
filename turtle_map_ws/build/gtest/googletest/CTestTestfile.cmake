f2d3eeb3e68cbf3295399dcc6e111069e024a047
Rasya <school.rasya@gmail.com>
2024-11-02 03:28:19 +0700

    Delete turtle_map_ws/build directory

--- turtle_map_ws/build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/rasya/Documents/ROBOTIK/ROS/tugas_turtle/turtle_map_ws/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
